1f051c7964b887be8e26a514a6d0fcef05ade149
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-28 15:37:37 -0800

    create verifyUser function

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,8 +5,17 @@ import {
   getUser,
   getUsers,
 } from "../controllers/user.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next) => {
+  res.send("Hello user, you are now logged in :)")
+}) 
+
+router.get("/checkuser/:id", verifyUser, (req,res,next) => {
+  res.send("Hello user, you are logged in and can delete your account.")
+}) 
 
 //UPDATE
 router.put("/:id", updateUser);
