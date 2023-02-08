948458cda8bbd762371ea8d183692f3b172124b7
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-07 19:27:19 -0800

    add destination to search, style search box

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,8 +1,16 @@
 import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
 
 const List = () => {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+
   return (
     <div>
       <Navbar />
@@ -13,10 +21,14 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Destination</label>
-              <input type="text"></input>
+              <input placeholder={destination} type="text"></input>
             </div>
             <div className="lsItem">
               <label>Check-in Date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                  date[0].endDate,
+                  "MM/dd/yyyy"
+                )}`}</span>
             </div>
           </div>
           <div className="listResult"></div>
