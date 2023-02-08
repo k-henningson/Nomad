e8604955a48a4c858cfa762a966173636ad87ec1
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-07 19:43:20 -0800

    add state as placeholder and set min number for inputs

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -55,15 +55,30 @@ const List = () => {
               </div>
               <div className="lsOptionItem">
                 <span className="lsOptionText">Adult</span>
-                <input type="number" className="lsOptionInput" />
+                <input
+                  type="number"
+                  min={1}
+                  className="lsOptionInput"
+                  placeholder={options.adult}
+                />
               </div>
               <div className="lsOptionItem">
                 <span className="lsOptionText">Children</span>
-                <input type="number" className="lsOptionInput" />
+                <input
+                  type="number"
+                  min={0}
+                  className="lsOptionInput"
+                  placeholder={options.children}
+                />
               </div>
               <div className="lsOptionItem">
                 <span className="lsOptionText">Room</span>
-                <input type="number" className="lsOptionInput" />
+                <input
+                  type="number"
+                  min={1}
+                  className="lsOptionInput"
+                  placeholder={options.room}
+                />
               </div>
             </div>
           </div>
