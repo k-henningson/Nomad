e16401dbb35035bb5623559f6063748c8417dbd1
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-23 14:51:00 -0800

    calculate days and price based off search options

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -23,8 +23,16 @@ const Hotel = () => {
 
   const { data, loading, error } = useFetch(`/hotels/find${id}`);
 
+  const { dates, options } = useContext(SearchContext);
 
-  const { dates } = useContext(SearchContext);
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
+
+  const days = dayDifference(dates[0].endDate, dates[0].startDate);
 
   const handleOpen = (index) => {
     setSlideNumber(index);
@@ -108,13 +116,13 @@ const Hotel = () => {
                 <p className="hotelDesc">{data.desc}</p>
               </div>
               <div className="hotelDetailsPrice">
-                <h1>Perfect for a 5-night stay!</h1>
+                <h1>Perfect for a {days}-night stay!</h1>
                 <span>
                   Located in the heart of New York, this hotel has an excellent
                   location score of 9.1
                 </span>
                 <h2>
-                  <b>$2345</b> (5 nights)
+                  <b>${days * data.cheapestPrice * options.room}</b> ({days} nights)
                 </h2>
                 <button>Reserve or Book Now!</button>
               </div>
